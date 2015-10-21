51be56f61cd8101200883893d2e7db321ea33e9d
Benjamin Alderfer <Ben@Benjamins-MacBook-Pro.local>
2015-10-20 20:27:23 -0400

    Use gulp for sass compiliation rather than grunt

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,24 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+// ... variables
+var autoprefixerOptions = {
+  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
+
+gulp.task('sass', function () {
+  return gulp
+    .src('./sass/base.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest('./public/stylesheets/'));
+});
 
 gulp.task('js', function(){
   browserify('./public/javascripts/src/app.jsx')
@@ -13,7 +31,8 @@ gulp.task('js', function(){
 });
 
 gulp.task('watch', function() {
-  gulp.watch("public/javascripts/src/**/*.jsx", ["js"])
+  gulp.watch("public/javascripts/src/**/*.jsx", ["js"]);
+  gulp.watch("sass/**/*.scss", ["sass"]);
 });
 
-gulp.task('default', ['js', 'watch']);
+gulp.task('default', ['js', 'sass', 'watch']);
