ae9e44604f0d5af3209a00ee4e240efb50183ab5
Miranda Mott <mmott@purdue.edu>
2015-11-13 00:30:43 -0500

    Miranda's profile

--- public/javascripts/src/components/profile/Profile.jsx
+++ public/javascripts/src/components/profile/Profile.jsx
@@ -1,6 +1,11 @@
 var React = require('react');
 var WebAPIUtils = require('../../utils/WebAPIUtils.js');
 var ProfileStore = require('../../stores/ProfileStore.js');
+var Row = require('react-bootstrap/lib/Row');
+var Col = require('react-bootstrap/lib/Col');
+var Feed = require('../feed/Feed.jsx');
+var Panel = require('react-bootstrap/lib/Panel');
+var Image = require('react-bootstrap/lib/Image');
 
 var getStateFromStores = function() {
   return {
@@ -31,8 +36,20 @@ module.exports = React.createClass({
   render: function() {
     return (
       <div className="profile">
-        <h1>Profile for {this.state.data.firstName} {this.state.data.lastName}</h1>
-        <img src={this.state.data.photoUrl} alt=""/>
+        <Row>
+          <Col sm={4}>
+            <Panel header={this.state.data.firstName}>
+              <Image src={this.state.data.photoUrl} rounded />
+              <div>{this.state.data.age}</div>
+              <div>{this.state.data.location}</div>
+              <div>{this.state.data.organization}</div>
+              <div>{this.state.data.jobTitle}</div>
+            </Panel>
+          </Col>
+          <Col sm={8}>
+            <Feed/>
+          </Col>
+        </Row>
       </div>
     );
   }
